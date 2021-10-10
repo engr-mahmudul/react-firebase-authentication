8f0f973527d34a19fcec551bcb4e511d63216a49
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-10 10:21:45 +0600

    UI Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import image1 from '../src/images/authentication.png'
+import initializationFirebase from './Firebase/firebase.initialize';
+initializationFirebase()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent-div">
+      <div className='left-div'>
+        <img src={image1} alt="" />
+      </div>
+      <div className="right-div">
+
+        <h1 style={{ marginTop: '90px', fontWeight: '800' ,color:"#018B97"}}> User Authentication</h1>
+        <div className='form-div'>
+          <div>
+            <form >
+              <input type="text" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <input style={{marginTop:'10px',backgroundColor:'#018B97',color:'white'}} type="submit" value='Register'/>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfJgmCLmJ_yAJRY2a3gJBaDCtZX5zi3DE",
+    authDomain: "react-firebase-authentic-91304.firebaseapp.com",
+    projectId: "react-firebase-authentic-91304",
+    storageBucket: "react-firebase-authentic-91304.appspot.com",
+    messagingSenderId: "940033382467",
+    appId: "1:940033382467:web:32b22a1277d10571269cf6"
+  };
+  export default firebaseConfig;
