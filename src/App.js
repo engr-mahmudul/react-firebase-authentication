fc5bdc704d19dcccb84b6d633ec1f1f340b0b0b5
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-10 11:24:30 +0600

    email,password and onsubmit handler add

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import initializationFirebase from './Firebase/firebase.initialize';
 initializationFirebase()
 
 function App() {
+  const formSubmitHandler =(e)=>{
+    e.preventDefault()
+    console.log('Form submit clicked')
+  }
+  const emailHandler = (event) =>{
+    console.log(event.target.value)
+  }
+  const passwordHandler = (event) =>{
+    console.log(event.target.value)
+  }
   return (
     <div className="parent-div">
       <div className='left-div'>
-        <img src={image1} alt="" />
+        <img src={image1} alt="" style={{width:'80%'}} />
       </div>
       <div className="right-div">
 
         <h1 style={{ marginTop: '90px', fontWeight: '800' ,color:"#018B97"}}> User Authentication</h1>
         <div className='form-div'>
           <div>
-            <form >
-              <input type="text" placeholder="Email" />
-              <input type="password" placeholder="Password" />
+            <form onSubmit={formSubmitHandler}>
+              <input onBlur={emailHandler} type="text" placeholder="Email" />
+              <input onBlur={passwordHandler}  type="password" placeholder="Password" />
               <input style={{marginTop:'10px',backgroundColor:'#018B97',color:'white'}} type="submit" value='Register'/>
             </form>
           </div>
